d6a66498095192e85ba64e1963cf314af6f5c5e1
Jiho Lee <baleen37@gmail.com>
2025-11-29 21:49:04 +0900

    feat: add Locale enum with all supported LoL locales

---
+++ tests/unit/test_locale.py
@@ -0,0 +1,29 @@
+from lol_replay_recorder.models.locale import Locale
+
+
+def test_locale_has_korean():
+    assert Locale.ko_KR == "ko_KR"
+
+
+def test_locale_has_english_us():
+    assert Locale.en_US == "en_US"
+
+
+def test_locale_has_japanese():
+    assert Locale.ja_JP == "ja_JP"
+
+
+def test_locale_values_are_strings():
+    assert isinstance(Locale.ko_KR.value, str)
+
+
+def test_locale_has_all_expected_locales():
+    expected_locales = {
+        "ja_JP", "ko_KR", "ar_AE", "cs_CZ", "de_DE", "el_GR",
+        "en_AU", "en_GB", "en_PH", "en_SG", "en_US",
+        "es_AR", "es_ES", "es_MX", "fr_FR", "hu_HU", "it_IT",
+        "pl_PL", "pt_BR", "ro_RO", "ru_RU", "th_TH", "tr_TR",
+        "vi_VN", "zh_MY", "zh_TW"
+    }
+    actual_locales = {locale.value for locale in Locale}
+    assert actual_locales == expected_locales
