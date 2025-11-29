f83df6a5a3f3f254f6e0361f7b51f67b2acec0f3
Jiho Lee <baleen37@gmail.com>
2025-11-29 21:45:59 +0900

    feat: add CustomError exception class

--- src/lol_replay_recorder/models/custom_error.py
+++ src/lol_replay_recorder/models/custom_error.py
@@ -3,3 +3,8 @@ class CustomError(Exception):
 
     def __init__(self, *args):
         super().__init__(*args)
+
+    def __str__(self):
+        if self.args:
+            return str(self.args[0])
+        return ""
